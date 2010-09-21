f0bd69d6fc67ec41860b829a920fd8fc0e963a0d
Ryan Smith and Vojto Rinik <vojto@rinik.net>
2010-09-21 08:47:07 -0400

    Added docs for QueueItem, cleanup

--- lib/active_harmony/queue_item.rb
+++ lib/active_harmony/queue_item.rb
@@ -10,6 +10,8 @@ module ActiveHarmony
     field :object_remote_id
     field :object_local_id
       
+    ##
+    # Processes queued item
     def process_item
       if kind == "push"
         self.process_push
@@ -20,6 +22,8 @@ module ActiveHarmony
       end
     end
     
+    ##
+    # Processes queued item of type push
     def process_push
       factory = "::#{object_type}".constantize
       local_object = factory.find(object_local_id)
@@ -30,6 +34,8 @@ module ActiveHarmony
       self.save
     end
     
+    ##
+    # Processes queued item of type pull
     def process_pull
       factory = "::#{object_type}".constantize
       syncer = factory.synchronizer
