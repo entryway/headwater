d7f913b92bb6ac41299d1e4889f0649ecf7de5ba
Ryan Smith and Vojto Rinik <vojto@rinik.net>
2010-09-22 11:25:05 -0400

    Changed configuration, added auto push configuration option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,28 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   layout "application"
   
-  after_filter do
+  after_filter :request_did_finish
+  
+  protected
+  
+  def request_did_finish
+    perform_auto_push
+  end
+  
+  def perform_auto_push
+    unless HW_AUTO_PUSH
+      Rails.logger.info("[Synchronization] Auto push is disabled.")
+      return
+    end
     queue = ActiveHarmony::Queue.instance
     if queue.should_run?
       fork do
+        Rails.logger.info("[Synchronization] Running queue ...")
         result = queue.run
-        Rails.logger.info("[Synchronization] Running queue: #{result.count}")
+        Rails.logger.info("[Synchronization] Items processed: #{result.count}")
       end
+    else
+      Rails.logger.info("[Synchronization] Queue is empty.")
     end
   end
 end
--- config/initializers/services.rb
+++ config/initializers/services.rb
@@ -1,14 +1,11 @@
-PIVOTAL_TRACKER_URL = "http://www.pivotaltracker.com/services/v3"
-PIVOTAL_TRACKER_API_KEY = "fe92da9b1a8932985b132e5e909de533" # Entryway
-# PIVOTAL_TRACKER_API_KEY = "467bc905766e089b3c37d5fd7100ce62" # Vojto
-HARVEST_SUBDOMAIN = "entryway"
+require File.join(Rails.root, 'config', 'headwater.rb')
 
 SERVICE_MANAGER = ActiveHarmony::ServiceManager.new
 
 # Harvest
 # ------------------------------------------
 harvest_service = ActiveHarmony::Service.new
-harvest_service.base_url = "https://#{HARVEST_SUBDOMAIN}.harvestapp.com"
+harvest_service.base_url = "https://#{HW_HARVEST_SUBDOMAIN}.harvestapp.com"
 harvest_service.header['Accept'] = 'application/xml'
 harvest_service.add_custom_url(:timeentry, :create, '/daily/add')
 harvest_service.add_custom_url(:timeentry, :update, 'daily/update/:id', :post)
@@ -23,7 +20,7 @@ SERVICE_MANAGER.add_service_for_identifier(harvest_service, :harvest)
 # Pivotal Tracker
 # ------------------------------------------
 tracker_service = ActiveHarmony::Service.new
-tracker_service.base_url = PIVOTAL_TRACKER_URL
-tracker_service.header['X-TrackerToken'] = PIVOTAL_TRACKER_API_KEY
+tracker_service.base_url = HW_PIVOTAL_TRACKER_URL
+tracker_service.header['X-TrackerToken'] = HW_PIVOTAL_TRACKER_API_KEY
 # Add service
 SERVICE_MANAGER.add_service_for_identifier(tracker_service, :tracker)
